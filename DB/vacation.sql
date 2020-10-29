82e45b463f52f50b560509562793ddf287522558
andreasN78 <45739821+andreasN78@users.noreply.github.com>
2020-10-29 23:51:12 +0200

    Add files via upload

--- DB/vacation.sql
+++ DB/vacation.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 24, 2020 at 11:48 PM
+-- Generation Time: Oct 29, 2020 at 11:31 PM
 -- Server version: 5.7.31-0ubuntu0.18.04.1
 -- PHP Version: 7.2.24-0ubuntu0.18.04.6
 
@@ -23,45 +23,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Application`
---
-
-CREATE TABLE `Application` (
-  `id_Application` int(11) NOT NULL,
-  `date_Submitted` date DEFAULT NULL,
-  `vacation_Start` date DEFAULT NULL,
-  `vacation_End` date DEFAULT NULL,
-  `days_Requested` int(4) DEFAULT NULL,
-  `application_Status` enum('pending','approved','rejected') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `Application`
---
-
-INSERT INTO `Application` (`id_Application`, `date_Submitted`, `vacation_Start`, `vacation_End`, `days_Requested`, `application_Status`) VALUES
-(1, '2020-10-29', '2020-11-01', '2020-11-05', 5, 'pending'),
-(2, '2020-11-01', '2020-11-15', '2020-11-19', 5, 'pending'),
-(3, '2020-12-05', '2020-12-15', '2020-12-26', 12, 'pending'),
-(4, '2021-01-01', '2021-01-07', '2021-01-12', 6, 'pending'),
-(5, '2020-02-15', '2020-10-16', '2020-10-20', 5, 'approved'),
-(6, '2020-02-15', '2020-10-16', '2020-10-20', 5, 'approved'),
-(7, '2020-09-14', '2020-09-28', '2020-10-01', 4, 'rejected'),
-(8, '2020-09-14', '2020-09-28', '2020-10-01', 4, 'rejected');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `User`
 --
 
 CREATE TABLE `User` (
   `user_Id` int(11) NOT NULL,
-  `user_Password` varchar(100) NOT NULL,
+  `user_Password` varchar(255) NOT NULL,
   `user_Firstname` varchar(45) DEFAULT NULL,
   `user_Lastname` varchar(70) DEFAULT NULL,
   `user_Email` varchar(70) DEFAULT NULL,
-  `user_Type` enum('employee','admin') NOT NULL
+  `user_Type` enum('Employee','Administrator') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -69,19 +40,15 @@ CREATE TABLE `User` (
 --
 
 INSERT INTO `User` (`user_Id`, `user_Password`, `user_Firstname`, `user_Lastname`, `user_Email`, `user_Type`) VALUES
-(1, '81dc9bdb52d04dc20036dbd8313ed055', 'Antreas', 'Neofytou', 'antreas8595@hotmail.com', 'employee'),
-(4, 'd93591bdf7860e1e4ee2fca799911215', 'Marios', 'Charalambides', 'marios@gmail.com', 'employee'),
-(5, 'd93591bdf7860e1e4ee2fca799911215', 'Marios', 'Charalambides', 'marios@gmail.com', 'employee');
+(16, '7e2feac95dcd7d1df803345e197369af4b156e4e7a95fcb2955bdbbb3a11afd8bb9d35931bf15511370b18143e38b01b903f55c5ecbded4af99934602fcdf38c', 'Andreas', 'Neofytou', 'biolaris3@gmail.com', 'Administrator'),
+(17, 'd404559f602eab6fd602ac7680dacbfaadd13630335e951f097af3900e9de176b6db28512f2e000b9d04fba5133e8b1c6e8df59db3a8ab9d60be4b97cc9e81db', 'Andreas', 'Neofytou', 'antreas8595@hotmail.com', 'Administrator'),
+(18, '3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79', 'Philippos', 'Christodoulou', 'philip@gmail.com', 'Employee'),
+(19, 'ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413', 'Test', 'Test2', 'test@test.com', 'Employee'),
+(20, 'ba3253876aed6bc22d4a6ff53d8406c6ad864195ed144ab5c87621b6c233b548baeae6956df346ec8c17f5ea10f35ee3cbc514797ed7ddd3145464e2a0bab413', 'TEst2', 'Test3', 'test@test.comtest', 'Employee');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `Application`
---
-ALTER TABLE `Application`
-  ADD PRIMARY KEY (`id_Application`);
 
 --
 -- Indexes for table `User`
@@ -94,15 +61,10 @@ ALTER TABLE `User`
 --
 
 --
--- AUTO_INCREMENT for table `Application`
---
-ALTER TABLE `Application`
-  MODIFY `id_Application` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
 -- AUTO_INCREMENT for table `User`
 --
 ALTER TABLE `User`
-  MODIFY `user_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
